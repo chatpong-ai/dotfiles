576e3fa080e156b25e5cc2978296cf6ee3e05041
chatpong voranartaksorn <chatpong.v@192.168.0.109>
2024-09-19 06:37:42 +0700

    commit
    1

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,3 +1,6 @@
+alias reload!=". ~/.zshrc"
+
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
 alias rmdd="rm -rf ~/Library/Developer/Xcode/DerivedData"
+alias cls='clear' # Good 'ol Clear Screen command
